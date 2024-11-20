619fb7075f944a48582cb1daba84fc3b62214548
Abigail <abigailtcameron@gmail.com>
2024-11-19 19:35:25 -0600

    deployed

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,26 @@
 import type { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox-viem";
+import "dotenv/config";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.27",
+
+  networks: {
+    sepolia: {
+      url: `https://sepolia-rollup.arbitrum.io/rpc`,
+      accounts: [process.env.PRIVATE_KEY ?? ''], 
+      chainId: 421614
+
+    }
+  },
+  etherscan: {
+    apiKey: {
+      arbitrumSepolia: process.env.ARBISCAN_API_KEY ?? '', // Replace with your Arbiscan API key
+    },
+  },
+  sourcify: {
+    enabled: true
+  }
 };
 
 export default config;
---
+++ scripts/deploy.ts
@@ -0,0 +1,19 @@
+import hre from "hardhat";
+
+async function main() {
+
+  const [deployerWalletClient] = await hre.viem.getWalletClients();
+  console.log("Deploying contract with address:", deployerWalletClient.account.address);
+
+  const publishContract = await hre.viem.deployContract("PublishRegistry");
+  console.log("Deployed to:", publishContract.address);
+
+
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+  console.error(error);
+  process.exit(1);
+});
